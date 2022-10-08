118a7cd7794f5c4512f53d68269ed3b30ebd2d21
Sangh0 <hop7311@naver.com>
2022-10-08 22:33:35 +0900

    update cifar10 dataset

---
+++ DINO/util/dataset.py
@@ -0,0 +1,60 @@
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.utils.data import DataLoader
+
+from augmentation import DataAugmentation
+
+
+def get_dataset(global_img_size=224, 
+                local_img_size=96, 
+                global_crops_scale=(0.4, 1.0), 
+                local_crops_scale=(0.05, 0.4), 
+                local_crops_number=8, 
+                batch_size=32):
+
+    train_transforms_ = DataAugmentation(
+        global_img_size=global_img_size,
+        local_img_size=local_img_size,
+        global_crops_scale=global_crops_scale,
+        local_crops_scale=local_crops_scale,
+        local_crops_number=local_crops_number,
+    )
+
+    test_transforms_ = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+
+    train_data = dsets.CIFAR10(
+        root='./data', 
+        train=True, 
+        download=True, 
+        transform=train_transforms_
+    )
+
+    train_loader = DataLoader(
+        train_data,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=True,
+    )
+
+    test_data = dsets.CIFAR10(
+        root='./data', 
+        train=False, 
+        download=True, 
+        transform=test_transforms_
+    )
+
+    test_loader = DataLoader(
+        test_data,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=True,
+    )
+
+    return {
+        'train': train_loader,
+        'test': test_loader,
+    }
