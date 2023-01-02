1f0fd19d64a4368d597293a6ab12efc7186442e5
Sangh0 <hop7311@naver.com>
2023-01-02 15:43:32 +0900

    modify inputs and outputs

--- SimCLR/model.py
+++ SimCLR/model.py
@@ -5,8 +5,10 @@ from torchvision import models
 
 class ResNet50(nn.Module):
     """ Base Encoder Network """
-    def __init__(self, feature_dim: int=128):
+    def __init__(self, feature_dim: int=128, proj: bool=False):
         super(ResNet50, self).__init__()
+
+        self.proj = proj
 
         self.encoder = models.resnet50(weights=None, num_classes=feature_dim)
         mlp_dim = self.encoder.fc.in_features
@@ -14,21 +16,20 @@ class ResNet50(nn.Module):
         # base encoder
         self.encoder.fc = nn.Identity()
 
-        # encoder containing projection head
+        # add projection head
         self.projection = nn.Sequential(
             nn.Linear(mlp_dim, mlp_dim),
             nn.ReLU(),
             nn.Linear(mlp_dim, feature_dim),
         )
 
-    def forward(self, x_i, x_j):
-        out_i = self.encoder(x_i)
-        out_j = self.encoder(x_j)
+    def forward(self, x):
+        out = self.encoder(x)
 
-        proj_i = self.projection(out_i)
-        proj_j = self.projection(out_j)
+        if not self.proj:
+            proj = self.projection(out)
         
-        return {
-            'encoder': [out_i, out_j],
-            'projection': [proj_i, proj_j],
-        }
+            return out, proj
+
+        else:
+            return out
